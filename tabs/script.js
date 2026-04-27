a93d92a22644eaa58fa8a487f7622d9c53160fc8
gourmandqq <jordon1539@gmail.com>
2026-04-27 17:15:00 +0300

    tabs

---
+++ tabs/script.js
@@ -0,0 +1,8 @@
+const tabBtns = document.querySelectorAll('.tab-btn');
+
+tabBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        tabBtns.forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+    });
+});
